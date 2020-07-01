c64c92d8cb0f1b47914a5dca8aa4af2c6283ef04
Auden RovelleQuartz <auden.rovellequartz@gmail.com>
2020-07-01 03:10:56 +0000

    update .gitignore file for template case, and settings.py

--- settings.py
+++ settings.py
@@ -1,32 +1,14 @@
-"""
-This is an optional file that defined app level settings such as:
-- database settings
-- session settings
-- i18n settings
-This file is provided as an example:
-"""
 import os
 
 # db settings
 APP_FOLDER = os.path.dirname(__file__)
 APP_NAME = os.path.split(APP_FOLDER)[-1]
+
 # DB_FOLDER:    Sets the place where migration files will be created
 #               and is the store location for SQLite databases
 DB_FOLDER = os.path.join(APP_FOLDER, "databases")
 DB_URI = "sqlite://storage.db"
-DB_POOL_SIZE = 1
-
-# send email on regstration
-VERIFY_EMAIL = False
-
-# account requires to be approved ?
-REQUIRES_APPROVAL = False
-
-# email settings
-SMTP_SERVER = None
-SMTP_SENDER = "you@example.com"
-SMTP_LOGIN = "username:password"
-SMTP_TLS = False
+DB_POOL_SIZE = 20
 
 # session settings
 SESSION_TYPE = "cookies"
@@ -39,31 +21,11 @@ LOGGERS = [
     "warning:stdout"
 ]  # syntax "severity:filename" filename can be stderr or stdout
 
-# single sign on Google (will be used if provided)
-OAUTH2GOOGLE_CLIENT_ID = None
-OAUTH2GOOGLE_CLIENT_SECRET = None
-
-# single sign on Google (will be used if provided)
-OAUTH2FACEBOOK_CLIENT_ID = None
-OAUTH2FACEBOOK_CLIENT_SECRET = None
-
 # enable PAM
 USE_PAM = False
 
-# enable LDAP
-USE_LDAP = False
-LDAP_SETTINGS = {
-    "mode": "ad",
-    "server": "my.domain.controller",
-    "base_dn": "ou=Users,dc=domain,dc=com",
-}
-
 # i18n settings
 T_FOLDER = os.path.join(APP_FOLDER, "translations")
-
-# Celery settings
-USE_CELERY = False
-CELERY_BROKER = "redis://localhost:6379/0"
 
 # try import private settings
 try:
